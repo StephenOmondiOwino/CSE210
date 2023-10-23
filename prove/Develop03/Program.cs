83c2cb816f91db84e071ea9d2bd7cf486926065b
stephen Omondi <132571105+stephen@users.noreply.github.com>
2023-10-23 12:44:28 +0300

    first commit

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,45 @@
 using System;
 
+
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture= new Scripture();
+    
+
+        Console.Write("John 2:3");
+        scripture.Referance = Console.ReadLine();
+        Console.Write("And hereby we do know that we know him, if we keep his commandments");
+        scripture.Text = Console.ReadLine();
+
+        Console.Clear();
+        DisplayScripture(scripture);
+
+        while(true)
+        {
+            Console.Write("\nPress Enter to hide random words or type 'quit' to exit:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            break;
+
+            scripture.Text = scripture.HideRandomWords();
+            Console.Clear();
+            DisplayScripture(scripture);
+        }
+        }
+
+        static void DisplayScripture(Scripture scripture)
+        {
+
+    
+
+        Console.WriteLine($"Scripture Reference: {scripture.Reference}");
+        Console.WriteLine($"Scripture Text: {scripture.Text}");
+        
+
+        Console.WriteLine();
     }
 }
